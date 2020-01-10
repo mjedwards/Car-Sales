b544a521d598c9ed71ccde9160ee8ed57b095c95
mjedwards <jairedwards20@gmail.com>
2020-01-09 22:24:53 -0500

    first commit

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        className='button'
+        onClick={() => props.removeFeature(props.feature)}
+      >
+        X
+      </button>
       {props.feature.name}
+      {console.log(props.feature.name)}
     </li>
   );
 };
+
+// const mapStateToProps = state => {
+//   return {
+//     [car.feature]: state.car.feature
+//   };
+// };
+
+// export default connect(mapStateToProps, { addFeature })(AddedFeature);
 
 export default AddedFeature;
